2d9e2d0ed4b467c49965e64ca1bcbbd7bd5268bf
codixir <codixir@gmail.com>
2020-05-24 08:38:56 -0400

    reformat

--- script.js
+++ script.js
@@ -4,34 +4,34 @@ const content = [
 	{ id: 3, body: 'three'},
 ];
 
-	const carContent = document.querySelector('.car-content');
-	const initContent = content[0];
-	let currentContent = initContent;
+const carContent = document.querySelector('.car-content');
+const initContent = content[0];
+let currentContent = initContent;
 
-	carContent.innerHTML = `<div>${content[0].body}</div>`;
+carContent.innerHTML = `<div>${content[0].body}</div>`;
 
-  function next() {
-    let i = 0;
-    while(i < content.length) {  	
-      if (content[i].id === currentContent.id + 1) {
-        carContent.innerHTML = `<div>${content[i].body}</div>`;
-        currentContent = content[i];
-        break;
-      }
-
-      i++;
+function next() {
+  let i = 0;
+  while(i < content.length) {  	
+    if (content[i].id === currentContent.id + 1) {
+      carContent.innerHTML = `<div>${content[i].body}</div>`;
+      currentContent = content[i];
+      break;
     }
+
+    i++;
   }
+}
 
-  function prev() {
-    let i = content.length - 1;
-    while(i >= 0) {
-      if (content[i].id === currentContent.id - 1) {
-        carContent.innerHTML = `<div>${content[i].body}</div>`;
-        currentContent = content[i];
-        break;
-      }
-
-      i--;
+function prev() {
+  let i = content.length - 1;
+  while(i >= 0) {
+    if (content[i].id === currentContent.id - 1) {
+      carContent.innerHTML = `<div>${content[i].body}</div>`;
+      currentContent = content[i];
+      break;
     }
-  }	
+
+    i--;
+  }
+}	
